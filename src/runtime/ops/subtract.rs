162b49fcc81b359037ca187ad899a33137c08c44
Justin Chase <justin.m.chase@gmail.com>
2020-02-01 08:01:48 -0600

    subtract expression tests

--- src/runtime/ops/subtract.rs
+++ src/runtime/ops/subtract.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use super::super::super::ast::Expression;
 use super::super::{
   Scope,
@@ -24,5 +25,25 @@ pub fn subtract(scope: Scope, left: &Expression, right: &Expression) -> Result<V
       _ => Err(InvalidValueError)
     },
     _ => Err(InvalidValueError)
+  }
+}
+
+#[test]
+fn sub_tests() {
+  let values = vec![
+    (Value::Int(3), Value::Int(2), Ok(Value::Int(1))),
+    (Value::String("a".to_string()), Value::String("b".to_string()), Err(InvalidValueError)),
+    (Value::String("a".to_string()), Value::Int(0), Err(InvalidValueError)),
+    (Value::Int(0), Value::String("a".to_string()), Err(InvalidValueError)),
+    (Value::None, Value::None, Err(InvalidValueError)),
+
+    // tood: all combinations...
+  ];
+  for (l, r, v) in values.iter() {
+    let left = Expression::Literal(l.clone());
+    let right = Expression::Literal(r.clone());
+    let s = Scope::new(Rc::new(vec![]));
+    let res = subtract(s, &left, &right);
+    assert_eq!(res, *v);
   }
 }
