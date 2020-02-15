b32121f95ca9491a5daa23a2af7a2a6a9d5ab630
Justin Chase <justin.m.chase@gmail.com>
2020-02-15 12:39:14 -0600

    fix up warnings in a file

--- src/runtime/patterns/array.rs
+++ src/runtime/patterns/array.rs
@@ -1,9 +1,6 @@
-use std::rc::Rc;
-use super::super::super::ast::{
-  Pattern
-};
+
+use super::super::super::ast::Pattern;
 use super::super::{
-  Value,
   Scope,
   Match,
   transform,
@@ -30,30 +27,38 @@ pub fn array(start: Scope, pattern: &Option<Box<Pattern>>) -> Result<Match, Runt
     None => Err(RuntimeError::ScopeEmptyError)
   }
 }
+#[cfg(test)]
+mod tests {
+  use std::rc::Rc;
+  use super::super::super::super::ast::Pattern;
+  use super::super::super::{
+    Value,
+    Scope
+  };
 
-#[test]
-fn array_matches_empty_array() {
-  let s = Scope::new(Rc::new(vec![Value::Array(vec![])]));
-  let m = array(s, &None);
-  let res = m.unwrap();
-  assert_eq!(res.matched, true);
-  assert_eq!(res.value, Value::Array(vec![]));
-}
+  #[test]
+  fn array_matches_empty_array() {
+    let s = Scope::new(Rc::new(vec![Value::Array(vec![])]));
+    let m = super::array(s, &None);
+    let res = m.unwrap();
+    assert_eq!(res.matched, true);
+    assert_eq!(res.value, Value::Array(vec![]));
+  }
 
-#[test]
-fn array_matches_non_empty_array() {
-  let s = Scope::new(Rc::new(vec![Value::Array(vec![Value::Int(7)])]));
-  let m = array(s, &Some(Box::new(Pattern::Any)));
-  let res = m.unwrap();
-  assert_eq!(res.matched, true);
-  assert_eq!(res.value, Value::Array(vec![Value::Int(7)]));
-}
+  #[test]
+  fn array_matches_non_empty_array() {
+    let s = Scope::new(Rc::new(vec![Value::Array(vec![Value::Int(7)])]));
+    let m = super::array(s, &Some(Box::new(Pattern::Any)));
+    let res = m.unwrap();
+    assert_eq!(res.matched, true);
+    assert_eq!(res.value, Value::Array(vec![Value::Int(7)]));
+  }
 
-
-#[test]
-fn array_fails_if_non_empty_array_doesnt_match_pattern() {
-  let s = Scope::new(Rc::new(vec![Value::Array(vec![Value::Int(7)])]));
-  let m = array(s, &Some(Box::new(Pattern::Equal(Value::Int(11)))));
-  let res = m.unwrap();
-  assert_eq!(res.matched, false);
+  #[test]
+  fn array_fails_if_non_empty_array_doesnt_match_pattern() {
+    let s = Scope::new(Rc::new(vec![Value::Array(vec![Value::Int(7)])]));
+    let m = super::array(s, &Some(Box::new(Pattern::Equal(Value::Int(11)))));
+    let res = m.unwrap();
+    assert_eq!(res.matched, false);
+  }
 }
